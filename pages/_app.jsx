3fdd24edab9ba1b2cd1ce513843e48f6de5e7c6b
jorge-collins <jorge.collins@gmail.com>
2023-01-14 18:54:55 -0700

    Initial commit.

---
+++ pages/_app.jsx
@@ -0,0 +1,13 @@
+import '@/styles/globals.css'
+import { lightTheme } from '@/themes'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+
+export default function App({ Component, pageProps }) {
+
+  return (
+    <ThemeProvider theme={ lightTheme }>
+        <CssBaseline />
+        <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+const HomePage = () => {
+  return (
+    <Typography variant='h1'>Hola Mundo</Typography>
+  )
+}
+
+export default HomePage;
